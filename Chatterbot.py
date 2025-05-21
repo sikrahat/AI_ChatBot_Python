0290446af9723a3c9d8f3aa00ce6bdd94e24492f
Sikder Rahat <sikrahat@gmail.com>
2025-05-22 03:44:05 +1000

    Update Chatterbot.py

--- Chatterbot.py
+++ Chatterbot.py
@@ -6,7 +6,15 @@ chatbot = ChatBot('FreeBirdsBot')
 
 trainer = ListTrainer(chatbot)
 
-trainer.train(['Hi','Hello','How are you?','I am fine and You?','Greate','What are you Doing?','nothing just roaming around.'])
+trainer.train([
+	'Hi',
+	'Hello',
+	'How are you?',
+	'I am fine and You?',
+	'Greate',
+	'What are you Doing?',
+	'nothing just roaming around.'
+])
 
 while True:
 	input_data = input("You- ")
